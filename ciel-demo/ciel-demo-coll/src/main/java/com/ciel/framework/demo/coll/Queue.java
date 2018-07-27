188a22a47acc60a8cf10f9c9e4da446c1810d624
happy150923 <chengxia0714@sina.cn>
2018-07-27 19:27:05 +0800

    Signed-off-by: happy150923 <chengxia0714@sina.cn>

---
+++ ciel-demo/ciel-demo-coll/src/main/java/com/ciel/framework/demo/coll/Queue.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C), 2002-2018, 苏宁易购电子商务有限公司
+ * FileName: PropertiesConfigureGetterTest.java
+ * Author:   18063410
+ * Date:     2018/7/19 20:16
+ * Description: //模块目的、功能描述
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.ciel.framework.demo.coll;
+
+/**
+ * <br>
+ *
+ * @author 18063410
+ * @date: 2018/7/19 20:16
+ * @see
+ * @since 盘古/大润发
+ */
+public class Queue {
+
+
+
+}
